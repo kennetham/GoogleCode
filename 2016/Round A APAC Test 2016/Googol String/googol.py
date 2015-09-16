41d412464abfcc9ac1b0ea063298211a46512a4c
Kenneth Ham <kenneth.ham@comp.nus.edu.sg>
2015-09-16 22:48:18 +0800

    [Solution] Googol.py
    Signed-off-by: Kenneth Ham <kenneth.ham@comp.nus.edu.sg>

---
+++ 2016/Round A APAC Test 2016/Googol String/googol.py
@@ -0,0 +1,20 @@
+test_case = input()
+
+def switch(binary):
+    temp = ""
+    for i in binary:
+        if i == "0":
+            temp += "1"
+        else:
+            temp += "0"
+    return temp
+
+binary = ""
+for i in xrange(17):
+    temp = binary[::-1]
+    temp = switch(temp)
+    binary = binary + "0" + temp
+
+for idx in xrange(test_case):
+    k = input()
+    print "Case #" + str(idx + 1) + ": " + binary[k - 1]
