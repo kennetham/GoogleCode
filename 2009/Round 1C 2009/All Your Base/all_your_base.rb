7556249a9f5db91d31f036de4cb0ea6b9efcd177
Kenneth Ham <kenneth.ham@comp.nus.edu.sg>
2015-09-16 23:24:26 +0800

    [Solution] All Your Base
    - Simple code
    - Unable to solve certain test cases
    
    Signed-off-by: Kenneth Ham <kenneth.ham@comp.nus.edu.sg>

---
+++ 2009/Round 1C 2009/All Your Base/all_your_base.rb
@@ -0,0 +1,34 @@
+#!/bin/ruby
+
+test_case = gets.chomp
+k = 1
+
+for i in 0...test_case.to_i
+  input = gets.chomp
+
+  begin
+    if Integer(input)
+      puts "Case ##{k}: " + input.to_s(2)
+    end
+  rescue
+    size = input.size
+    queue = Array.new
+    digit = 2
+
+    for j in 0...size do
+      if (j == 0)
+        queue.push(1)
+      elsif (j == 1)
+        queue.push(0)
+      else
+        queue.push(digit)
+        digit += 1
+      end
+    end
+
+    result = queue.join("")
+    puts "Case ##{k}: " + result.to_i(size).to_s
+  end
+
+  k += 1
+end
