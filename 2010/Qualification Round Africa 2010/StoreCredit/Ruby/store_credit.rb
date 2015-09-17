dcfc4c5c31e514fa59ca649e8b9372372dd44264
Kenneth Ham <kenneth.ham@comp.nus.edu.sg>
2015-09-17 21:53:46 +0800

    [Updated] Minor change to algorithm
    Signed-off-by: Kenneth Ham <kenneth.ham@comp.nus.edu.sg>

--- 2010/Qualification Round Africa 2010/StoreCredit/Ruby/store_credit.rb
+++ 2010/Qualification Round Africa 2010/StoreCredit/Ruby/store_credit.rb
@@ -10,13 +10,31 @@ for i in 0...test_case.to_i
   item = l_item.split
   item_index = Array.new
   is_found = false
+  has_duplicates = false
 
   hash = Hash.new { |h, k| h[k] = [] }
+  duplicate_items = Hash.new { |h, k| h[k] = [] }
+  item.each_index { |i| duplicate_items[item[i]] << i unless 1 == item.count(item[i]) }
   item.each_with_index do |val, idx|
     hash[val] << idx
   end
 
-  item.permutation(2) { |a, b|
+  if item.uniq.length == item.length
+    has_duplicates = false
+  else
+    has_duplicates = true
+  end
+
+  store_items = item.product(item)
+
+  # result = []
+  # item.each do |x|
+  #   item.reverse.each do |y|
+  #     result << [x, y]
+  #   end
+  # end
+
+  item.combination(2) { |a, b|
     if (a.to_i + b.to_i == store_credit.to_i && is_found == false)
       if (hash[a] == hash[b])
         item_index.push(hash[a][0] + 1, hash[b][1] + 1)
