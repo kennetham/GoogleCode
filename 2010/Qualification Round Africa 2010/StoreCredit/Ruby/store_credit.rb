04394314512864c6f9b72f64a2ab8b5800f69db4
Kenneth Ham <kenneth.ham@comp.nus.edu.sg>
2015-09-17 18:16:06 +0800

    [Updated] StoreCredits solution bug fix
    - Fix duplicate numbers beside each other
    - Solve some test cases, but mostly incorrect
    
    Signed-off-by: Kenneth Ham <kenneth.ham@comp.nus.edu.sg>

--- 2010/Qualification Round Africa 2010/StoreCredit/Ruby/store_credit.rb
+++ 2010/Qualification Round Africa 2010/StoreCredit/Ruby/store_credit.rb
@@ -11,14 +11,31 @@ for i in 0...test_case.to_i
   item_index = Array.new
   is_found = false
 
+  hash = Hash.new { |h, k| h[k] = [] }
+  item.each_with_index do |val, idx|
+    hash[val] << idx
+  end
+
   item.permutation(2) { |a, b|
+    # puts item.index("4")
+    # puts "INDEX:: #{a}[#{item.index("#{a.to_i}") + 1}] + #{b}[#{item.index("#{b.to_i}") + 1}]"
     if (a.to_i + b.to_i == store_credit.to_i && is_found == false)
-      item_index.push(item.index("#{a.to_i}") + 1, item.index("#{b.to_i}") + 1)
+      if (hash[a] == hash[b])
+        item_index.push(hash[a][0] + 1, hash[b][1] + 1)
+      else
+        item_index.push(item.index("#{a.to_i}") + 1, item.index("#{b.to_i}") + 1)
+      end
+
       is_found = true
     elsif (is_found == true)
       if (item_index[0] > item.index("#{a.to_i}") + 1)
         item_index.clear
-        item_index.push(item.index("#{a.to_i}") + 1, item.index("#{b.to_i}") + 1)
+
+        if (hash[a] == hash[b])
+          item_index.push(hash[a][0] + 1, hash[b][1] + 1)
+        else
+          item_index.push(item.index("#{a.to_i}") + 1, item.index("#{b.to_i}") + 1)
+        end
       end
     end
   }
